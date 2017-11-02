a993da6cb6cd290a20c25b67b5e8823df7ba64ed
buccanr <fuentes.lucas972@gmail.com>
2017-11-02 11:16:12 +0100

    Added main game window

--- SoftwareDevelopmentProcessProject/src/main/java/Main.java
+++ SoftwareDevelopmentProcessProject/src/main/java/Main.java
@@ -1,7 +1,33 @@
 package main.java;
 
-public class Main {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage)       //generate the main game window
+    {
+        try
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("/main/java/Board.fxml"));
+            Scene scene = new Scene(root, 1280, 720);
+            primaryStage.setTitle("Test App");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
+        //launch(args);     //open the main game window
         //Game game = new Game();
         //game.Init(number_of_players);
         //game.Launch();

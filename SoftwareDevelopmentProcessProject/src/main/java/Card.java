53e474e3aec39872c9494344f49e1f38f92f24c2
Yassin Hassan <yh@yassinhassan.com>
2017-11-06 09:33:46 +0100

    Adding abstract methods getRace, and adding javadoc

--- SoftwareDevelopmentProcessProject/src/main/java/Card.java
+++ SoftwareDevelopmentProcessProject/src/main/java/Card.java
@@ -10,5 +10,17 @@ public abstract class Card {
     // ********** SETTERS **********
 
     // ********** METHODS **********
+
+    /**
+     * Method to apply the power of the card
+     * @param game : the current game
+     * @param player : the player that owns the card
+     */
     public abstract void ApplyPower(Game game, Player player);
+
+    /**
+     * Method to get the Race of the card
+     * @return the race of the card
+     */
+    public abstract Race getRace();
 }
